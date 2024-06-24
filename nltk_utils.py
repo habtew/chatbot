4570f398f6bd2d91f7a675ff14923ce19ca28156
habtew <habtamuwolde92@gmail.com>
2024-06-24 10:30:09 +0300

    create dataset

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,5 +1,5 @@
 import nltk
-
+import numpy as np
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
 
@@ -11,4 +11,17 @@ def stem(word):
     return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_sentence, all_words):
-    pass
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+    return bag
+
+
+# sent = ["hello", "how", "are", "you"]
+# all_wods = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+
+# bag = bag_of_words(sent, all_wods)
+# print(bag)
--- train.py
+++ train.py
@@ -1,7 +1,12 @@
 import json
 from nltk_utils import tokenize, stem, bag_of_words
-
+import numpy as np
 # open trainging data
+# import torch
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
 
 with open('intents.json', 'r') as f:
     intents = json.load(f)
@@ -23,4 +28,36 @@ ignore_words = ['?', '.', ',', '!']
 all_words = [stem(w) for w in all_words if w not in ignore_words]
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
-print(tags)
+
+# separate data
+x_train = []
+y_train = []
+for (tokenized_sentence, tag) in xy:
+    bag = bag_of_words(tokenized_sentence, all_words)
+    x_train.append(bag)
+
+    label = tags.index(tag)
+    y_train.append(label) #crossentropy lebel
+
+X_train = np.array(x_train)
+Y_train = np.array(y_train)
+
+
+class ChatDataSet(Dataset):
+    def __init__(self):
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = Y_train
+
+    
+    # to access dataset with index
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+    
+    def __len__(self):
+        return self.n_samples
+    
+# hyper parameters
+batch_size = 8
+dataset = ChatDataSet()
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
